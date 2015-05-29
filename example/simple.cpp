83ea1a0dd1546e357d5bd89c92d72bbc761209a4
Jukka Sarjanen <jukka-pekka.sarjanen@kone.com>
2015-05-29 20:37:25 +0300

    simple example added and cmake files fixed so that example compiles

---
+++ example/simple.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "simplemqttclient.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    int rc,val;
+    SimpleMqttClient client("localhost","dummy");
+    rc = client.connectToMqttServer();
+    if (rc)
+    {
+       std::cout << "Connect failed rc:"<< rc << std::endl;
+       exit(-1);
+    }
+    rc = client.subscribeFromMqttServer("simple/#");
+
+    if (rc)
+    {
+       std::cout << "subscribe failed rc:"<< rc << std::endl;
+       exit(-1);
+    }
+
+    std::cout << "Publish few messages and exit..."<< std::endl;
+    val=0;
+
+    while(val < 5)
+    {
+        val++;
+        std::ostringstream s;
+        s << "Hello from simpleclient" << val;
+        client.publishToMqttServer("simple/data",s.str());
+        sleep(2);
+    }
+
+    return 0;
+}
